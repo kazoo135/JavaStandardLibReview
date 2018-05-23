6a8ec711f579335426b07c70f0529819d5cb09df
kazoo135 <kazoo6723@gmail.com>
2018-05-23 16:26:14 -0400

    initial commit with .gitignore and test app

---
+++ src/com/kazoo/basics/HelloAgain.java
@@ -0,0 +1,11 @@
+package com.kazoo.basics;
+
+public class HelloAgain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Again");
+
+	}
+
+}
